90607029e18ac68e104c23a5e295497852484492
seba17962 <isebsebastiansilva@gmail.com>
2023-11-21 16:37:47 +0000

    task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- Task 4
 -- script that prepares a MySQL server for the project
-CREATE database if not exists hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE hbnb_test_db;
-CREATE user if not exists 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost'
