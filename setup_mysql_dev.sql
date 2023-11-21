df9581ef21dff78596db94505b5f8286b396bfc0
NicoV00 <6328@holbertonstudents.com>
2023-11-21 08:47:07 -0800

    added server with mysql to the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Server for the project.
+-- hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant prall privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Gran SELECT privilege on performance_schema.
+GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
