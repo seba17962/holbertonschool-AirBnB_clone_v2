b099cc05100b38131f50a91747e572e1cb547f7d
seba17962 <isebsebastiansilva@gmail.com>
2023-11-21 16:33:50 +0000

    script that prepares a MySQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,5 @@
+-- Task 4
+-- script that prepares a MySQL server for the project
 CREATE database if not exists hbnb_test_db;
 USE hbnb_test_db;
 CREATE user if not exists 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
