72e740922887b280abd6d6a1765fce37c5c52155
seba17962 <isebsebastiansilva@gmail.com>
2023-11-21 16:53:32 +0000

    fix space

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,4 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant prall privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Gran SELECT privilege on performance_schema.
-GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
