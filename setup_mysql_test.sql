20153033538b1fafd321e6eca0841991b1578be2
seba17962 <isebsebastiansilva@gmail.com>
2023-11-21 16:32:12 +0000

    script that prepares a MySQL

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+CREATE database if not exists hbnb_test_db;
+USE hbnb_test_db;
+CREATE user if not exists 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost'
