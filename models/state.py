2ff8fedf005038102a7ecd93db4e645ff45d6493
NicoV00 <6328@holbertonstudents.com>
2023-11-24 11:38:44 -0800

    added state py fixed

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ State class created."""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        @ property
+        def cities(self):
+            """Return all cities."""
+            from models import storage
+            my_list = []
+            for i in list(storage.all(City).values()):
+                if self.id == i.state_id:
+                    my_list.append(i)
+            return my_list
