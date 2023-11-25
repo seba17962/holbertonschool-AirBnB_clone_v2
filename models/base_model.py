5a901c90c64cc18d3158f3940c2a8dcbd49248df
NicoV00 <6328@holbertonstudents.com>
2023-11-25 11:51:43 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -2,60 +2,57 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
 
 Base = declarative_base()
+
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
+    """Base class"""
+    id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Init new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             for key, value in kwargs.items():
-                if not hasattr(self, key):
-                    setattr(self, key, value)
+                if key == 'updated_at' or key == 'created_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """string representation of instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with current time"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """Convert instance into dict"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
-            del dictionary['_sa_instance_state']
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
+        """Delete instance"""
         from models import storage
-
         storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" State for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
+""" State Module for HBNB project """
 from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.city import City
 
 
-class State(BaseModel):
-    """ State class created."""
+class State(BaseModel, Base):
+    """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
@@ -17,7 +17,7 @@ class State(BaseModel):
     else:
         @ property
         def cities(self):
-            """Return all cities."""
+            """Return all cities linked to this relationship."""
             from models import storage
             my_list = []
             for i in list(storage.all(City).values()):
