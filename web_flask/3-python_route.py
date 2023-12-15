eeafcb5ea4f45b00c3e09fdd4a4ac9dbf43d0801
seba17962 <isebsebastiansilva@gmail.com>
2023-12-15 23:38:44 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    task 2
+
+    """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """_summary_
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """_summary_
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """_summary_
+
+    """
+    format_text = text.replace('_', ' ')
+    return f'C {format_text}'
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text= "is cool"):
+    format_python_text = text.replace('_', ' ')
+    return f'Python {format_python_text}'
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
